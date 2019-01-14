7a4b96671bf1b22a1f89f48bceba91a08d4b859d
Chongnaitor <42983270+Chongnaitor@users.noreply.github.com>
2019-01-14 13:50:25 -0600

    Add files via upload

---
+++ Ejercicio1Cap1.cpp
@@ -0,0 +1,12 @@
+#include<pch.h>
+#include<iostream>
+#include<string>
+using namespace std; 
+inline void keep_window_open() { char ch; cin >> ch; }
+int main()
+{
+	cout << "Hola programacion!\n";
+	cout << "Aqui vamos!\n";
+	return 0;
+
+}
