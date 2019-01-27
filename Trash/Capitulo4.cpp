060ca7d87e592ff314d91e28c56ad99ff18eb227
idv18c.jchong@uartesdigitales.edu.mx <idv18c.jchong@uartesdigitales.edu.mx>
2019-01-26 23:06:48 -0600

    Nuevo codigo en eñ que lñlevo 10 horas

--- Trash/Capitulo4.cpp
+++ Trash/Capitulo4.cpp
@@ -107,15 +107,232 @@ int ejercicio2()
 
 	}
 }
-int main()
+int ejercicio3()
 {
 	double suma=0;
 	vector<double>Ciudades{150,89,70,53,98};
 	for (double i : Ciudades) suma += i;
 	{
-		cout << suma;
+		cout << "Esta es la suma total de todas las distancias entre ciudades " << suma<<"km\n";
+
 	}
+	sort(Ciudades);
+	int elemento = Ciudades[1];
+	int elemento2 = Ciudades[3];
 	
+
+	if (Ciudades.front() <= elemento);
+	{
+		cout <<"La distancia mas pequena entre ciudades es :"<< Ciudades.front() + elemento<<"\n";
+	}
+	if (Ciudades.back() >= elemento2)
+	{
+		cout << "La distancia mas grande entre ciudades es: " << Ciudades.back() + elemento2 << "\n";
+	}
 	cin.ignore();
 		cin.get();
+		return 485;
+
 }
+int main()
+{
+	vector<int>Numeros;
+	vector<int>Numeros2;
+	vector<int>Numeros3;
+	vector<int>Numeros4;
+	int b = 101;
+	for (int i = 0; i != b;)
+	{
+		Numeros.push_back(i);
+		i++;
+	}
+	cout << "Bienvenido al juego en el que la maquina te adivinara el numero en el que estas pensando del 1 al cien\n";
+	cout << "Tu numero es mayor a 50?:\n";
+	string NumeroDeUsuario0;
+	cin >> NumeroDeUsuario0;
+	if (NumeroDeUsuario0 == "Si" || NumeroDeUsuario0 == "si")
+	{
+		for (int s = 0; s < Numeros.size(); s++)
+		{
+			if (Numeros[s] >= 50)
+			{
+				Numeros2.push_back(s);
+			}
+		}
+	}
+	if (NumeroDeUsuario0 == "Si" || NumeroDeUsuario0 == "si")
+	{
+		string Mayor75;
+		cout << "Tu numero es mayor a 75?\n";
+		cin >> Mayor75;
+		if (Mayor75 == "Si" || Mayor75 == "si")
+		{
+			for (int y = 0; y < Numeros2.size(); y++)
+			{
+				if (Numeros2[y] >= 75)
+				{
+					Numeros3.push_back(y);
+				}
+			}
+		}
+
+		else if (Mayor75 == "No" || Mayor75 == "no")
+		{
+			for (int i = 0; i < Numeros2.size(); i++)
+			{
+				if (Numeros2[i] <= 75)
+				{
+					Numeros3.push_back(i);
+				}
+			}
+		}
+	}
+
+
+	if (NumeroDeUsuario0 == "No" || NumeroDeUsuario0 == "no")
+	{
+		for (int s = 0; s < Numeros.size(); s++)
+		{
+			if (Numeros[s] <= 50)
+			{
+				Numeros2.push_back(s);
+			}
+		}
+
+
+	}
+	if (NumeroDeUsuario0 == "No" || NumeroDeUsuario0 == "no")
+	{
+		string Mayor25;
+		cout << "Tu numero es mayor a 25?\n";
+		cin >> Mayor25;
+		if (Mayor25 == "Si" || Mayor25 == "si")
+		{
+			for (int y = 0; y < Numeros2.size(); y++)
+			{
+				if (Numeros2[y] >= 25)
+				{
+					Numeros3.push_back(y);
+				}
+			}
+		}
+		else if (Mayor25 == "no" || Mayor25 == "No")
+		{
+			for (int y = 0; y < Numeros2.size(); y++)
+			{
+				if (Numeros2[y] <= 25)
+				{
+					Numeros3.push_back(y);
+				}
+			}
+		}
+	}
+
+
+	string MultiplosLocos;
+	if (Numeros3.back() == 25)
+		cout << "Tu numero es multiplo de 5?\n";
+	cin >> MultiplosLocos;
+	if (MultiplosLocos == "Si" || MultiplosLocos == "si")
+	{
+		cout << "Tu numero es mayor 15?\n";
+		cin >> MultiplosLocos;
+		if (MultiplosLocos == "Si" || MultiplosLocos == "si")
+		{
+			cout << "Tu numero es 20?\n";
+			cin >> MultiplosLocos;
+			if (MultiplosLocos == "Si" || MultiplosLocos == "si")
+			{
+				cout << "Wiiiiiiiii\n";
+			}
+			else
+			{
+				cout << "Tu numero es 25";
+			}
+
+		}
+		else if (MultiplosLocos == "No" || MultiplosLocos == "no")
+		{
+			cout << "Tu numero es 15?\n";
+			cin >> MultiplosLocos;
+			if (MultiplosLocos == "Si" || MultiplosLocos == "si")
+			{
+				cout << "Wiiii!!\n";
+			}
+			else if (MultiplosLocos == "No" || MultiplosLocos == "no")
+			{
+				cout << "Tu numero es 10?\n";
+				cin >> MultiplosLocos;
+				if (MultiplosLocos == "Si" || MultiplosLocos == "si")
+				{
+					cout << "Wiiiiiiiii\n";
+				}
+				else
+				{
+					cout << "Tu numero es 5";
+				}
+			}
+		}
+	}
+	else if (MultiplosLocos == "No" || MultiplosLocos == "no")
+	{
+		cout << "Tu numero esta entre el 1 y el 13";
+		cin >> MultiplosLocos;
+		if (MultiplosLocos == "Si" || MultiplosLocos == "si")
+		{
+			cout << "Tu numero es multiplo de 3?\n";
+			cin >> MultiplosLocos;
+			if (MultiplosLocos == "si" || "Si")
+			{
+				cout << "Tu numero es mayor a 9?\n";
+				cin >> MultiplosLocos;
+				if (MultiplosLocos == "si" || MultiplosLocos == "Si")
+				{
+					cout << "Tu numero es 12\n";
+
+				}
+				else if (MultiplosLocos == "no" || MultiplosLocos == "No")
+				{
+					if (MultiplosLocos == "no" || MultiplosLocos == "No")
+					{
+						cout << "Tu numero es 9?\n";
+						cin >> MultiplosLocos;
+						if (MultiplosLocos == "si" || MultiplosLocos == "Si")
+						{
+							cout << "Yay";
+						}
+						else if (MultiplosLocos == "no" || MultiplosLocos == "No")
+						{
+							cout << "Tu numero es 6?";
+							cin >> MultiplosLocos;
+							if (MultiplosLocos == "si" || MultiplosLocos == "Si")
+							{
+
+								cout << "Yay";
+							}
+							else if (MultiplosLocos == "no" || MultiplosLocos == "No")
+							{
+								cout << "Tu numero es 3";
+							}
+						}
+					}
+				}
+				else if (MultiplosLocos == "No" || MultiplosLocos == "no")
+				{
+
+				}
+
+				else if (MultiplosLocos == "No" || MultiplosLocos == "no")
+				{
+
+				}
+			}
+		}
+	}
+}
+		
+
+
+		
+
+	
